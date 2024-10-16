95ab6af94c9c243fed869cd66af57c73030d1360
Sergei <ser.koloskov13@gmail.com>
2024-10-17 00:45:11 +0300

    Sprint 9 / Theme 2 / Lesson 4 - Added SingleImageView with an image

---
+++ imageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  imageFeed
+//
+//  Created by MacBookPro on 17.10.2024.
+//
+
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
